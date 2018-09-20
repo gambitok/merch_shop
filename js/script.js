d19ca42f59b45b6e894e43fd29c1d356bc691ed0
gambitok <gambitokgd@gmail.com>
2018-09-20 14:19:27 +0300

    added OwlCarousel plugin and `Designer products block`

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-    $("#main").load("tpl/catalogue.html");
+    $("#main").load("tpl/catalogue.html",function() {
+        startOwl();
+    });
+
 });
+
+function startOwl() {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        margin: 10,
+        nav: true,
+        dots: false,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+}
